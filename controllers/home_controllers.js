86ee5b898a81e0cb1c89d43aa88512939164b6b8
Joy <joywadhonkar4@gmail.com>
2023-02-06 18:04:11 +0530

    create views and assets folder and modify the contollers and views as per requirenment

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,3 +1,5 @@
 module.exports.home=(req,res)=>{
-    return res.send("<h1>Log In | Sign In</h1>");
+    return res.render('home',{
+        title:"Home"
+    })
 }
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express=require('express');
 const port=8000;
 const app=express();
+const path=require('path');
 
+app.use(express.static("./assets"));
+
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname, '/views'));
 
 // use express router
 app.use("/",require("./routes/index"));
